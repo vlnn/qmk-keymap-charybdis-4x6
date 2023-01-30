c9495f5030a7fc9b12c16cebe36069ca9e1277a0
Volodymyr Anokhin <vlnn-github@proton.me>
2023-01-30 16:22:43 +0200

    Remove hex

--- vlnn/keymap.c
+++ vlnn/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MEH(KC_ESC), LALT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,   KC_H, RSFT_T(KC_J), RGUI_T(KC_K), RCTL_T(KC_L), RALT_T(KC_SCLN), KC_QUOT,
         KC_LCTL, LT(3,KC_Z), KC_X, KC_C, KC_V, KC_B,                                 KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_LBRC,
                                KC_BSPC, KC_DEL, KC_ENT,                              KC_ENT, TD(TD_SPC_TAB),
-                               KC_LALT, KC_LGUI,                                     LT(1,KC_LPRN)),
+                               KC_LALT, KC_LGUI,                                     LT(1,S(KC_LPRN))),
     [1] = LAYOUT_charybdis_4x6(
         QK_BOOT, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_UNDS,
         EE_CLR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                   KC_LBRC, KC_P7, KC_P8, KC_P9, KC_RBRC, KC_NO,
