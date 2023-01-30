9dcd3b4d4f3507f5414097c43492ca4d8e6cfff6
Volodymyr Anokhin <vlnn-github@proton.me>
2023-01-30 16:13:12 +0200

    Reformat and tap dance for space/tab

--- vlnn/rules.mk
+++ vlnn/rules.mk
@@ -12,13 +12,13 @@ BOOTLOADER = caterina
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = yes        # Console for debug
+CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-TAP_DANCE_ENABLE = no
+TAP_DANCE_ENABLE = yes
 
 AUDIO_SUPPORTED = no        # Audio is not supported.
 RGB_MATRIX_SUPPORTED = no   # RGB matrix is supported and enabled by default.
