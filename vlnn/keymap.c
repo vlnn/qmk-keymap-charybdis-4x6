2459d7a5de89b595bee3dde7d003ff1033a057f8
Volodymyr Anokhin <vlnn-github@proton.me>
2023-02-02 13:09:17 +0200

    Higher tapping term for the comfort

--- vlnn/keymap.c
+++ vlnn/keymap.c
@@ -141,8 +141,8 @@ const uint16_t PROGMEM circ[] = {KC_Y, KC_U, COMBO_END};
 const uint16_t PROGMEM ampr[] = {KC_U, KC_I, COMBO_END};
 const uint16_t PROGMEM past[] = {KC_I, KC_O, COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
-    COMBO(circ, KC_CIRC),
-    COMBO(ampr, KC_AMPR),
+    COMBO(circ, KC_LBRC),
+    COMBO(ampr, KC_RBRC),
     COMBO(past, KC_PAST),
 };
 
@@ -152,8 +152,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                                        KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
         HYPR_T(KC_ESC), LALT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F),KC_G, KC_H, RSFT_T(KC_J), RGUI_T(KC_K), RCTL_T(KC_L), RALT_T(KC_SCLN), KC_QUOT,
         TD(TD_OPENBRACE), LT(3,KC_Z), KC_X, KC_C, KC_V, KC_B,                        KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(TD_OPENBRACE),
-                               KC_BSPC, KC_DEL, KC_F13,                              RSFT_T(KC_ENT), TD(TD_SPC_TAB),
-                               KC_LALT, KC_F14,                                      LT(1,KC_PGDN)),
+                               KC_BSPC, KC_DEL, KC_F13,                              RSFT_T(KC_ENT), LT(1,KC_SPC),
+                               KC_TAB, KC_F14,                                       KC_PGDN),
     [1] = LAYOUT_charybdis_4x6(
         QK_BOOT, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC,                           KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PLUS,
         EE_CLR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                   KC_LBRC, KC_P7, KC_P8, KC_P9, KC_RBRC, KC_NO,
